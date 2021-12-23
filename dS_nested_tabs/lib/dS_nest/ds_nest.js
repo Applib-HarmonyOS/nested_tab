ba8a563b117ccbf59d289539f75709ed54200c49
devashishkumar709 <57445475+devashishkumar709@users.noreply.github.com>
2021-12-23 18:01:22 +0530

    Library developed by Devashish & Sahil

---
+++ dS_nested_tabs/lib/dS_nest/ds_nest.js
@@ -0,0 +1,35 @@
+export default {
+    data: {
+    },
+    props: {
+
+//        images
+        img1:'',
+        img2:'',
+        img3:'',
+        img4:'',
+
+//        header text
+        txt1:'',
+        txt2:'',
+        txt3:'',
+        txt4:'',
+//        sub header text
+        atxt1:'',
+        atxt2:'',
+        atxt3:'',
+        atxt4:'',
+        btxt1:"",
+        btxt2:"",
+        btxt3:"",
+        btxt4:"",
+        ctxt1:"",
+        ctxt2:"",
+        ctxt3:"",
+        ctxt4:"",
+        dtxt1:"",
+        dtxt2:"",
+        dtxt3:"",
+        dtxt4:"",
+    },
+}
---
+++ entry/src/ohosTest/java/com/example/nested_tabs_library/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.example.nested_tabs_library;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.example.nested_tabs_library", actualBundleName);
+    }
+}
